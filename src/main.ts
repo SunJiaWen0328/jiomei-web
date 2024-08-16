595c2acb25b61ce9bc4216cfa3935ce0e24b81c0
sunjiawen <2096035911@qq.com>
2024-08-16 20:03:17 +0800

    更新部分代码

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'popper.js'
+import 'bootstrap'
+import { VueMasonryPlugin } from 'vue-masonry'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(VueMasonryPlugin)
+
+app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,44 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BaseLayOut from '@/layouts/base.layout.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/auth/login.vue'),
+  },
+  {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: BaseLayOut,
+    redirect: '/home',
+    meta: {
+      title: '首页',
+    },
+    children: [
+      {
+        path: 'home',
+        name: 'home-root',
+        component: () => import('@/views/home/index.vue'),
+      }
+    ]
+  },
+  {
+    path: '/explore',
+    name: 'Explore',
+    component: BaseLayOut,
+    redirect: { name: 'explore' },
+    meta: {
+      title: '发现',
+    },
+    children: [
+      {
+        path: '',
+        name: 'explore',
+        component: () => import('@/views/explore/index.vue'),
+      }
+    ]
   },
   {
     path: '/about',
